796b4d856363619289f24f9dac806285b3721983
jaspric <prickettjw@gmail.com>
2016-03-10 21:40:30 -0800

    Functional menus, ajax navigation, main layout done

--- js/script.js
+++ js/script.js
@@ -1,8 +1,27 @@
 $(document).ready(function() {
+//ajax navigation
+  var hash = window.location.hash;
+  var href = $('a[href="'+hash+'"]').attr('dest');
+  $('.body-container').load(href);
+
+//ajax nav for
+  $(window).on('hashchange', function(){
+    var hash = window.location.hash;
+    var href = $('a[href="'+hash+'"]').attr('dest');
+    $('.body-container').load(href);
+  })
+
+//slide function for dropdown menus
   $(".dropdown").hover(function(){
     $(this).children('.sub-nav').slideDown();
   },
     function(){
       $(this).children('.sub-nav').slideUp();
     });
+
+//slide menu down for phone menu
+  $("#menu-toggle").click(function(){
+    $(".navbar-full").animate({height: 'toggle'}, 300);
+  });
+
 });
