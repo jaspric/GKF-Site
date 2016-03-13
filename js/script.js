f1a6582ad633aece44ba3d1af7767375e50f8002
jaspric <prickettjw@gmail.com>
2016-03-13 00:50:07 -0800

    Nav UI fixes, CSS fixes

--- js/script.js
+++ js/script.js
@@ -1,15 +1,21 @@
+
+
 $(document).ready(function() {
-//ajax navigation
+
+  /*
+  
   var hash = window.location.hash;
   var href = $('a[href="'+hash+'"]').attr('dest');
   $('.body-container').load(href);
 
-//ajax nav for
+
   $(window).on('hashchange', function(){
     var hash = window.location.hash;
     var href = $('a[href="'+hash+'"]').attr('dest');
     $('.body-container').load(href);
   })
+
+  */
 
 //slide function for dropdown menus
   $(".dropdown").hover(function(){
